12584908fbeca65f64f97c2bc4846291c44a8f9f
Saloni Agrawal <saloniagrawal250797@gmail.com>
2022-04-09 17:49:48 +0530

    jszip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,8 @@ function loadFile(url, callback) {
 }
 
 const App = () => {
+  const finalZip = new PizZip();
+
   const generateDocument = (item) => {
     loadFile("http://127.0.0.1:8080/Template.docx", function (error, content) {
       try {
@@ -28,11 +30,24 @@ const App = () => {
           mimeType:
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         });
-        saveAs(out, `${item[1]}.docx`);
+        blobToBase64(out, function (binaryData) {
+          finalZip.file(`${item[1]}.docx`, binaryData, { base64: true });
+        });
+        //saveAs(out, `${item[1]}.docx`);
       } catch (e) {
         console.log(e.message);
       }
     });
+  };
+
+  const blobToBase64 = (blob, callback) => {
+    var reader = new FileReader();
+    reader.onload = function () {
+      var dataUrl = reader.result;
+      var base64 = dataUrl.split(",")[1];
+      callback(base64);
+    };
+    reader.readAsDataURL(blob);
   };
 
   const onButtonClick = (e) => {
@@ -45,8 +60,12 @@ const App = () => {
       const wsName = wb.SheetNames[0];
       const ws = wb.Sheets[wsName];
       const list = utils.sheet_to_json(ws, { header: 1 });
-      console.log(list);
       list.forEach(generateDocument);
+      PizZipUtils.getBinaryContent();
+      const content = finalZip.generate({
+        type: "blob",
+      });
+      saveAs(content, "output.zip");
     };
     reader.readAsBinaryString(file);
   };
